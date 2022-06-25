f0939c9bd7728d1da9780996f8100fbca0982577
Shivaraj Bakale <shivaraj.sb@byjus.com>
2022-06-25 22:08:57 +0530

    Fixed all the issues with react-virtualized package

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
-
+/* eslint-disable no-promise-executor-return */
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import {AutoSizer, InfiniteLoader, Table, Column} from 'react-virtualized';
+import { generateFakeData } from './utils';
 
 function App() {
-  return(
+  const [rows,setRows]=useState(generateFakeData(200));
+  const handleIsRowLoaded = ({ index }: { index: number }) => !!rows[index]
+
+  const handleLoadMoreRows=()=>new Promise((resolve)=>resolve([1,2,3]))
+
+  return (
     <div>
-      <h1>Hello World this is Shivaraj Bakale</h1>
+      <InfiniteLoader
+        isRowLoaded={handleIsRowLoaded}
+        loadMoreRows={handleLoadMoreRows}
+        rowCount={rows.length}
+      >
+        {({ onRowsRendered, registerChild }) => (
+          <AutoSizer>
+            {({ width }) => (
+              <Table
+                ref={registerChild}
+                onRowsRendered={onRowsRendered}
+                rowClassName="table-row"
+                headerHeight={40}
+                width={width}
+                rowHeight={40}
+                height={400}
+                rowCount={rows.length}
+                rowGetter={({ index }) => rows[index]}
+              >
+                <Column label="Id" dataKey="id" width={width * 0.2} />
+                <Column dataKey="name" label="Name" width={width * 0.2} />
+                <Column
+                  label="Last Name"
+                  dataKey="lastName"
+                  width={width * 0.2}
+                />
+              </Table>
+            )}
+          </AutoSizer>
+        )}
+      </InfiniteLoader>
     </div>
-  );
+  )
 }
+
 export default App;
